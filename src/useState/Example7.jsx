2fa57dbe3ce2790bb9c9dcb48c7d0a1920ac6f91
ssushilgit <shresthasushil741@gmail.com>
2024-08-22 00:54:07 +0545

    useState example with object

--- src/useState/Example7.jsx
+++ src/useState/Example7.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from 'react'
 
 const Example7 = () => {
-    const [count, setCount] = useState(0);
-    const [name, setName] = useState("");
+    // const [count, setCount] = useState(0);
+    // const [name, setName] = useState("");
+    const [details, setDetails] = useState({
+        count: 0,
+        name: ""
+    });
+
+    // const increaseCounter = () =>{
+    //     setCount(count + 1) //increase by 1
+    // }
+    const onChange = (e) => {
+        setDetails((prev) => ({
+            ...prev,
+            name: e.target.value 
+        }));
+    };
 
     const increaseCounter = () =>{
-        setCount(count + 1) //increase by 1
+        setDetails((prev) => ({
+            ...prev,
+            count: prev.count + 1 ,
+        }));
     }
-    const onChange =(e)=>{
-        setName(e.target.value)
-    }
+
+
   return (
     <div>
-        <input type="text" onChange={onChange} />
-        <h1> {name} has clicked {count} times! </h1>
+        <input type="text" onChange={onChange} style={{padding: "10px 20px"  }}/>
+        <h1> {details.name} has clicked {details.count} times! </h1>
         <button onClick={increaseCounter}>Increase</button>
     </div>
   )
